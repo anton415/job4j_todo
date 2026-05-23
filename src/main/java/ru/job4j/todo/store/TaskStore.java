9b8e499654389cd66f1ff4f161d0d237db3fd18c
Anton Serdyuchenko <anton415@gmail.com>
2026-05-23 13:16:13 +0300

    6. Лямбды и шаблон Command [#49295]

--- src/main/java/ru/job4j/todo/store/TaskStore.java
+++ src/main/java/ru/job4j/todo/store/TaskStore.java
@@ -1,49 +1,42 @@
 package ru.job4j.todo.store;
 
 import lombok.RequiredArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Task;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-import java.util.function.Function;
 
 @Repository
 @RequiredArgsConstructor
 public class TaskStore {
 
-    private final SessionFactory sf;
+    private final CrudRepository crudRepository;
 
     public List<Task> findAll() {
-        return tx(session -> session
-                .createQuery("from Task order by created desc", Task.class)
-                .list());
+        return crudRepository.query("from Task order by created desc", Task.class);
     }
 
     public List<Task> findByDone(boolean done) {
-        return tx(session -> session
-                .createQuery("from Task where done = :done order by created desc", Task.class)
-                .setParameter("done", done)
-                .list());
+        return crudRepository.query(
+                "from Task where done = :done order by created desc",
+                Task.class,
+                Map.of("done", done)
+        );
     }
 
     public Optional<Task> findById(int id) {
-        return tx(session -> Optional.ofNullable(session.get(Task.class, id)));
+        return crudRepository.tx(session -> Optional.ofNullable(session.get(Task.class, id)));
     }
 
     public Task save(Task task) {
-        tx(session -> {
-            session.save(task);
-            return task;
-        });
+        crudRepository.run(session -> session.persist(task));
         return task;
     }
 
     public boolean update(Task task) {
-        return tx(session -> session
+        return crudRepository.tx(session -> session
                 .createQuery("""
                         update Task
                         set title = :title,
@@ -59,7 +52,7 @@ public class TaskStore {
     }
 
     public boolean setDone(int id) {
-        return tx(session -> session
+        return crudRepository.tx(session -> session
                 .createQuery("""
                         update Task
                         set done = true
@@ -70,27 +63,12 @@ public class TaskStore {
     }
 
     public boolean delete(int id) {
-        return tx(session -> session
+        return crudRepository.tx(session -> session
                 .createQuery("""
                         delete from Task
                         where id = :id
                         """)
                 .setParameter("id", id)
                 .executeUpdate() > 0);
-    }
-
-    private <T> T tx(Function<Session, T> command) {
-        Transaction transaction = null;
-        try (Session session = sf.openSession()) {
-            transaction = session.beginTransaction();
-            T result = command.apply(session);
-            transaction.commit();
-            return result;
-        } catch (Exception exception) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            throw exception;
-        }
     }
 }
--- src/main/java/ru/job4j/todo/store/UserStore.java
+++ src/main/java/ru/job4j/todo/store/UserStore.java
@@ -1,64 +1,40 @@
 package ru.job4j.todo.store;
 
 import lombok.RequiredArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.User;
 
+import java.util.Map;
 import java.util.Optional;
-import java.util.function.Function;
 
 @Repository
 @RequiredArgsConstructor
 public class UserStore {
 
-    private final SessionFactory sf;
+    private final CrudRepository crudRepository;
 
     public Optional<User> findByLogin(String login) {
-        return tx(session -> session
-                .createQuery("from TodoUser where login = :login", User.class)
-                .setParameter("login", login)
-                .list()
-                .stream()
-                .findFirst());
+        return crudRepository.optional(
+                "from TodoUser where login = :login",
+                User.class,
+                Map.of("login", login)
+        );
     }
 
     public Optional<User> findByLoginAndPassword(String login, String password) {
-        return tx(session -> session
-                .createQuery("""
+        return crudRepository.optional(
+                """
                         from TodoUser
                         where login = :login
                           and password = :password
-                        """, User.class)
-                .setParameter("login", login)
-                .setParameter("password", password)
-                .list()
-                .stream()
-                .findFirst());
+                        """,
+                User.class,
+                Map.of("login", login, "password", password)
+        );
     }
 
     public User save(User user) {
-        tx(session -> {
-            session.save(user);
-            return user;
-        });
+        crudRepository.run(session -> session.persist(user));
         return user;
-    }
-
-    private <T> T tx(Function<Session, T> command) {
-        Transaction transaction = null;
-        try (Session session = sf.openSession()) {
-            transaction = session.beginTransaction();
-            T result = command.apply(session);
-            transaction.commit();
-            return result;
-        } catch (Exception exception) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            throw exception;
-        }
     }
 }
